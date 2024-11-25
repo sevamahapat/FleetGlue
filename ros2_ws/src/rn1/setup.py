c2296489848de57fd8d0127f038d1d2c90c6cd65
Seva Mahapatra <smahapat@andrew.cmu.edu>
2024-11-25 17:30:30 -0500

    Initial commit

---
+++ ros2_ws/src/rn1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'rn1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='smahapat',
+    maintainer_email='smahapat@andrew.cmu.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'rn1 = rn1.rn1:main'
+        ],
+    },
+)
